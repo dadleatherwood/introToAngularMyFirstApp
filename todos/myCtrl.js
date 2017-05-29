6ed6046d5bfdd63fa19aeec08c954be99236517b
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-05-29 12:10:39 -0500

    add a button to strike through an element using ng-class

--- todos/myCtrl.js
+++ todos/myCtrl.js
@@ -1,3 +1,9 @@
 angular.module('todoApp').controller('todoCtrl', function($scope){
-  $scope.test = 'I am working!'
+  $scope.todos = ["add todos", "make them display", "complete todo", "delete"];
+
+  $scope.addTodo = function(todo){
+    $scope.todos.push(todo)
+    $scope.newTodo = "";
+  };
+
 });
